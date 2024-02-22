0da4ad9f5bae34d4e51cb0c4d8c6cddaf447d055
Nico B <nicolasbrailo@gmail.com>
2024-02-22 08:20:49 +0000

    update genscript

--- search.js
+++ search.js
@@ -7,19 +7,15 @@ function togglesearch() {
 }
 
 function triggerSearch(e) {
-  console.log("SRC")
   if (e.preventDefault) e.preventDefault();
   const url = "https://github.com/search?type=code&q=repo%3Anicolasbrailo%2Fnicolasbrailo.github.io%20";
   const q = document.getElementById('sitesearch_q').value;
-  console.log("q")
   document.location = url + encodeURI(q);
   return false;
 }
 
 if (document.getElementById('sitesearch').attachEvent) {
   document.getElementById('sitesearch').attachEvent("submit", triggerSearch);
-  console.log("w")
 } else {
   document.getElementById('sitesearch').addEventListener("submit", triggerSearch);
-  console.log("X")
 }
